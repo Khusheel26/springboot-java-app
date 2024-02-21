54636526dadb3d2a0d24a167e729c66e4d85233d
Khusheel26 <khusheel26@gmail.com>
2024-02-21 22:01:53 +0530

    Added default files using spring initializer

---
+++ src/test/java/com/octobit8/springbootjavaapp/SpringbootJavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.octobit8.springbootjavaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
